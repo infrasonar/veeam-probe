ca9886a275c3cf7eb52379ffa6a1e267f79cb42e
Jeroen van der Heijden <jeroen@cesbit.com>
2025-07-09 11:48:42 +0200

    health

--- lib/check/health.py
+++ lib/check/health.py
@@ -7,19 +7,19 @@ async def check_health(
         asset: Asset,
         asset_config: dict,
         config: dict) -> dict[str, list[dict[str, Any]]]:
-    req = '/Health'
+    req = '/health'
     resp = await query(asset, asset_config, asset_config, req)
     entries = resp.get('entries', {})
     configuration_db = entries.get('configurationDb', {}).get('status')
     nats = entries.get('nats', {}).get('status')
 
     item = {
-        'name': 'Health',  # str
+        'name': 'health',  # str
         'status': resp['status'],  # str
         'configurationDb': configuration_db,  # str?
         'nats': nats,  # str?
     }
 
     return {
-        'Health': [item]
+        'health': [item]
     }
